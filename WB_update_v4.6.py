4e8bac8d099485ee59ac6b2a64859d0e450b7669
AleksTikhonov <88837533+AleksTikhonov@users.noreply.github.com>
2021-08-12 16:35:38 +0300

    Add files via upload

---
+++ WB_update_v4.6.py
@@ -0,0 +1,168 @@
+# encoding: utf-8
+# 2019 R3
+SetScriptVersion(Version="19.5.112")
+
+import os
+import shutil
+import re
+import logging
+
+logging.basicConfig(filename='python.log',filemode='w',level=logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%H:%M:%S')
+
+wdpath=os.getcwd()
+
+for f in os.listdir(wdpath):
+    if f.endswith(".wbpz"):
+        projectname = os.path.splitext(f)[0]
+        break
+
+projectfiledirpath=wdpath + '\\' + projectname + '_files' 
+projecinsidepath=wdpath + '\\' + projectname + '_files\\dp0'
+projectfilepath=wdpath + '\\' + projectname + '.wbpj'
+projectarchivepath=wdpath + '\\' + projectname + '.wbpz'
+csvpath=wdpath + '\\' + projectname + '.csv'
+pattern = r"(P\d{1,3})\s-\s([a-zA-Z _.@0-9]+)\s?(\[.+?\])*"
+pattern_2 = r"DP\s.*"
+output_file_name = wdpath + '\\values\\values.csv'
+res = []
+val = {}
+
+def process_file(csvpath):
+    with open(csvpath, "r") as f:
+        for line in f:
+            _res = re.findall(pattern, line)
+            if len(_res) > 0:
+                for _r in _res:
+                    res.append(list(_r))
+            _r = re.match(pattern_2, line)
+            if _r is not None:
+                _val = line.strip().split(',')
+                _cnt = 0
+                for v in _val[1:]:
+                    res[_cnt].append(v)
+                    _cnt += 1
+
+def save_output():
+    cnt = 1
+  
+    with open(output_file_name, 'w') as f:
+        f.write("#,Name,Value,Target,Condition,Dimension,Description,Comments,\n")
+        for e in res:
+            a = str(cnt) + ',' + str(e[1].strip()) + ',' + str(e[3]) + ',' + ',' + ',' + str(e[2]) + ',' + ',' + ','
+            f.write(a)
+            f.write("\n")
+            cnt += 1
+
+def save_results():
+    
+    for root, dirs, files in os.walk(projectfiledirpath):
+        for file in files:
+            if file.endswith('.res'):
+                shutil.copy(os.path.join(root, file), os.path.join(wdpath + '\\main_results\\cfd', file))
+                export_cfd_report()
+            if file.endswith('.gtm') or file.endswith('.mshdb'):
+                shutil.copy(os.path.join(root, file), os.path.join(wdpath + '\\main_results\\cfd\\mesh', file))                
+
+def export_report():
+    ExportReport(FilePath=wdpath + '\\main_results\\report.html')
+    for root, dirs, files in os.walk(projecinsidepath):
+        for file in files:
+            if file.startswith('Post') and file.endswith('.png'):
+                shutil.copy(os.path.join(root, file), os.path.join(wdpath + '\\pictures', file))
+    try:
+        shutil.copy(wdpath + '\\main_results\\report_images\\ProjectSchematic.png', wdpath + '\\pictures\\model_overview.png')
+    except:
+        pass
+
+def export_cfd_report():
+    for dir in os.listdir(projecinsidepath):
+        if dir.startswith('CFX') or dir.startswith('Post'):
+            dir = dir.replace('-',' ')
+            system1 = GetSystem(Name=dir)
+            results1 = system1.GetContainer(ComponentName="Results")
+            cfdResults1 = results1.GetCFDResults()
+            for fl in os.listdir(wdpath):
+                if fl.endswith(".cse"):
+                    cfdResults1.LoadReport = "Custom"
+                    cfdResults1.CustomReportTemplate = wdpath + '\\' +fl
+                    break
+                else:
+                    cfdResults1.LoadReport = "GenericReport"
+            cfdResults1.GenerateReport = True
+            cfdResults1.ReportLocationDirectory = wdpath + '\\main_results\\cfd'
+            resultsComponent1 = system1.GetComponent(Name="Results")
+            resultsComponent1.Update(AllDependencies=True)
+    
+    for root, dirs, files in os.walk(wdpath + '\\main_results\\cfd'):
+        for file in files:
+            if file.endswith('.png'):
+                shutil.copy(os.path.join(root, file), os.path.join(wdpath + '\\pictures', file))
+
+def make_dirs():
+    try:
+        os.mkdir(wdpath + '\\values')
+    except OSError:
+        pass
+    try:
+        os.mkdir(wdpath + '\\main_results')
+    except OSError:
+        pass
+    try:
+        os.mkdir(wdpath + '\\pictures')
+    except OSError:
+        pass
+    try:
+        os.mkdir(wdpath + '\\main_results\\cfd')
+    except OSError:
+        pass
+    try:
+        os.mkdir(wdpath + '\\main_results\\cfd\\mesh')
+    except OSError:
+        pass
+
+def change_parameters():
+    designPoint1 = Parameters.GetDesignPoint(Name="0")
+    for inp in os.listdir(wdpath):
+        if inp.startswith('input') and inp.endswith('.txt'):
+            f = open(wdpath + '\\' + inp)
+            for line in f.readlines():
+                a = line.split()
+                parameter1 = Parameters.GetParameter(Name=a[0])
+                try:
+                    designPoint1.SetParameterExpression(
+                    Parameter=parameter1,
+                    Expression=a[2] + " " + a[3])
+                except:
+                    designPoint1.SetParameterExpression(
+                    Parameter=parameter1,
+                    Expression=a[2])
+            f.close()
+
+Unarchive(
+    ArchivePath=projectarchivepath,
+    ProjectPath=projectfilepath,
+    Overwrite=True)
+
+Save(Overwrite=True)
+change_parameters()
+Update()
+Parameters.ExportAllDesignPointsData(FilePath=csvpath)
+make_dirs()
+export_report()
+export_cfd_report()
+save_results()
+
+Reset()
+
+try:
+    process_file(csvpath)
+    save_output()
+    os.remove(csvpath)
+except:
+    pass
+
+os.remove(projectfilepath)
+os.remove(projectarchivepath)
+shutil.rmtree(projectfiledirpath)
+
+logging.info("ALL DONE")
